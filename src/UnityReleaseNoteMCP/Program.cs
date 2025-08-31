c9f502a45188c542560671062b9787a0f37044c9
hanachiru <hayato.sato@dena.com>
2025-08-31 10:29:48 +0900

    create project

---
+++ src/UnityReleaseNoteMCP/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.AddConsole(o => o.LogToStandardErrorThreshold = LogLevel.Trace);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly()
+    .WithResourcesFromAssembly();
+
+await builder.Build().RunAsync();
